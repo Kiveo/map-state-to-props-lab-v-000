f68336c9d614973c81248a5e93a23095f7590101
Maverick Tesini <mavericktesini@gmail.com>
2018-09-17 02:33:35 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
 import manageUsers from './reducers/manageUsers'
-
-
 // add imports and code
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   // add imports and code
